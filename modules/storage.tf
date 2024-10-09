7e2c57739f6ea8e586f81c377751afed35f2e70f
nilamcd <71369200+nilam721@users.noreply.github.com>
2024-10-09 04:50:31 +0000

    Delete modules/storage.tf

--- modules/storage.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_storage_account" "storage" {
-  name = var.storageaccountname
-  resource_group_name = "TerraformRG"
-  location = "East US"
-  access_tier = var.tier 
-  account_replication_type = "LRS"
-  account_tier = "Standard"
-}
